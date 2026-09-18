79a2dd9460e5e6bb511e275445ce1a9d38ac6315
Felipe.Vram <fp562708@gmail.com>
2026-09-18 02:04:56 -0300

    add: remover nó folha,remover nó com um filho e alteração no menu

--- main.c
+++ main.c
@@ -1,83 +1,164 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct no
 {
     int valor;
     struct no *direita;
     struct no *esquerda;
-}NoArv;
+} NoArv;
 
-NoArv* inserir(NoArv *raiz, int num){
-    if(raiz == NULL){
+NoArv *inserir(NoArv *raiz, int num)
+{
+    if (raiz == NULL)
+    {
         NoArv *aux = malloc(sizeof(NoArv));
         aux->valor = num;
         aux->esquerda = NULL;
         aux->direita = NULL;
         return aux;
     }
-    else{
-        if (num<raiz->valor)
+    else
+    {
+        if (num < raiz->valor)
         {
             raiz->esquerda = inserir(raiz->esquerda, num);
         }
-        else{
+        else
+        {
             raiz->direita = inserir(raiz->direita, num);
         }
         return raiz;
     }
 }
 
-NoArv* buscar(NoArv *raiz, int num){
-    if(raiz!=NULL){
-        if(num == raiz->valor){
+NoArv *buscar(NoArv *raiz, int num)
+{
+    if (raiz != NULL)
+    {
+        if (num == raiz->valor)
+        {
             return raiz;
         }
-        else if(num < raiz->valor){
+        else if (num < raiz->valor)
+        {
             return buscar(raiz->esquerda, num);
         }
-        else{
+        else
+        {
             return buscar(raiz->direita, num);
         }
     }
     return NULL;
 }
 
-int main(){
+NoArv *remover(NoArv *raiz, int chave)
+{
+    if (raiz == NULL)
+    {
+        printf("Valor nao encontrado!");
+        return NULL;
+    }
+    else
+    {
+        if (raiz->valor == chave)
+        {
+            if (raiz->esquerda == NULL && raiz->direita == NULL)
+            {
+                free(raiz);
+                printf("No folha removido: %d", chave);
+                return NULL;
+            }
+            else
+            {
+                if (raiz->esquerda != NULL && raiz->direita != NULL)
+                {
+                    /* code */
+                }
+                else{
+                    NoArv *aux;
+                    if (raiz->esquerda != NULL)
+                    {
+                        aux = raiz->esquerda;
+                    }
+                    else{
+                        aux = raiz->direita;
+                    }
+                    free(raiz);
+                    printf("No com 1 filho removido: %d", chave);
+                    return aux;
+                }
+            }
+        }
+        else
+        {
+            if (chave < raiz->valor)
+            {
+                raiz->esquerda = remover(raiz->esquerda, chave);
+            }
+            else
+            {
+                raiz->direita = remover(raiz->direita, chave);
+            }
+        }
+    }
+}
+
+int main()
+{
     int opcao, valor;
-    NoArv *raiz = NULL;
-    
+    NoArv *busca, *raiz = NULL;
+
     do
     {
-        printf("\n\t 1 - Inserir valor \n\t 2 - Buscar valor \n\t 3 - Remover valor \n\t 4 - Percorrer arvore \n\t 5 - imprimir arvore \n\t 0 - Sair");
-        scanf("%d", &opcao );
+        printf("=======================>    MENU    <=======================\n");
+        printf("Selecione uma opcao (digite apenas o valor inteiro):\n");
+        printf(">(1) Inserir valor\n");
+        printf(">(2) Buscar valor\n");
+        printf(">(3) Remover valor\n");
+        printf(">(4) Percorrer arvore\n");
+        printf(">(5) Imprimir arvore\n");
+        printf(">(0) Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
 
         switch (opcao)
         {
         case 1:
-            printf("Digite um valor que deseja inserir: ");
+            printf("Digite um numero que deseja inserir: ");
             scanf("%d", &valor);
             raiz = inserir(raiz, valor);
-            if (raiz = inserir(raiz, valor))
-            {
-                printf("Valor inserido com sucesso!");
-            }
-            else{
-                printf("Tente novamente, ocorreu um problema no procedimento de insercao.");
-            }
+            printf("Numero inserido com sucesso!");
             break;
+
         case 2:
-            printf("Digite um valor que deseja buscar: ");
+            printf("Digite um numero que deseja buscar: ");
             scanf("%d", &valor);
-            buscar(raiz, valor);
-        default:
+            busca = buscar(raiz, valor);
+            if (busca)
+            {
+                printf("\n Numero encontrado: %d\n", busca->valor);
+            }
+            else
+            {
+                printf("Numero nao encontrado.\n");
+            }
             break;
+
+        case 3:
+            printf("Digite um valor que deseja remover: ");
+            scanf("%d", &valor);
+            raiz= remover(raiz, valor);
+            break;
+
+        default:
+            if (opcao != 0)
+            {
+                printf("Opcao invalida, tente novamente.");
+            }
         }
 
-    } while (opcao!=0);
-    
+    } while (opcao != 0);
 
     return 0;
-    
 }
